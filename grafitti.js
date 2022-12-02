476215de326a232184b7339ff5aac1a9c18d7f62
Timekyt <tymkon10@gmail.com>
2022-12-02 22:05:48 +0100

    init

--- grafitti.js
+++ grafitti.js
@@ -5,4 +5,19 @@ let person = {
     eyeColor: "blue"
 };
 
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+};
+let t = true;
+document.getElementById("action")
+
+function mmFunction() {
+    t = false
+};
+if (mmFunction = true) {
+    alert(change.document.getElementsByid("graw")[0].className = "draw1")
+} else {
+    alert(change.document.getElementsByidid("graw")[0].className = "draw2")
+};
+
 document.getElementById("box1").innerHTML = person.firstName + " " + person.lastName;
