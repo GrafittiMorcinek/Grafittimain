36f4d2e403fc4763d86e6f4555fd5ee3e5f14c4b
Timekyt <tymkon10@gmail.com>
2022-12-02 20:09:43 +0100

    init

--- grafitti.js
+++ grafitti.js
@@ -1,1 +1,8 @@
-var name = "username";
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+};
+
+document.getElementById("box1").innerHTML = person.firstName + " " + person.lastName;
